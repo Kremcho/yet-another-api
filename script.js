55de815a8eef1c38d19d63f27b09d324d211d3d8
Kremcho <hris0006@hz.nl>
2023-07-02 18:00:02 +0200

    some changes

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(() => {
+    // Get references to form and results div
+    const form = $('#searchForm');
+    const resultsDiv = $('#results');
+
+    // Listen for form submit event
+    form.submit((e) => {
+        e.preventDefault();
+
+        // Get input values
+        const countryInput = $('#country');
+        const yearInput = $('#year');
+        const country = countryInput.val().toUpperCase();
+        const year = yearInput.val();
+
+        // Send AJAX request to retrieve holidays
+        $.ajax({
+            url: `https://calendarific.com/api/v2/holidays?api_key=06c3246f322465d50f3c2e4c824905d58b1faf06&country=${country}&year=${year}`,
+            method: 'GET',
+            success: (data) => {
+                const holidays = data.response.holidays;
+
+                // Display holidays on the page
+                displayHolidays(holidays);
+            },
+            error: (error) => {
+                console.error('Error:', error);
+                resultsDiv.html('Error retrieving holidays. Please try again.');
+            }
+        });
+    });
+
+    // Function to display holidays
+    function displayHolidays(holidays) {
+        if (holidays.length === 0) {
+            // Display message if no holidays found
+            resultsDiv.html('No holidays found.');
+        } else {
+            // Clear previous results
+            resultsDiv.empty();
+
+            // Loop through holidays and create holiday boxes
+            holidays.forEach((holiday) => {
+                const holidayBox = $('<div>').addClass('holiday-box');
+
+                // Create paragraphs for holiday details
+                const dateParagraph = $('<p>').html(`<strong>Date:</strong> ${holiday.date.iso}`);
+                const nameParagraph = $('<p>').html(`<strong>Name:</strong> ${holiday.name}`);
+                const typeParagraph = $('<p>').html(`<strong>Type:</strong> ${holiday.type[0]}`);
+                const descriptionParagraph = $('<p>').html(`<strong>Description:</strong> ${holiday.description}`);
+
+                // Append paragraphs to holiday box
+                holidayBox.append(dateParagraph, nameParagraph, typeParagraph, descriptionParagraph);
+
+                // Append holiday box to results div
+                resultsDiv.append(holidayBox);
+            });
+        }
+    }
+});
